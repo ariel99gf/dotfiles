567f6cae76e556076d07886a73b979e214ae6016
Ariel Fonseca <50802265+ariel99gf@users.noreply.github.com>
2022-08-05 23:46:37 -0300

    Update httpd_1.sh

--- httpd_1.sh
+++ httpd_1.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-sudo reflector --country 'United States' --latest 200 --protocol https --age 24 --sort rate --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew
+sudo pacman-key --init
+sudo pacman-key --populate
+sudo pacman -Syy archlinux-keyring
 sudo pacman -Syuu --noconfirm
+sudo pacman -S reflector --noconfirm
+sudo reflector --country 'United States' --latest 200 --protocol https --age 24 --sort rate --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew
 sudo pacman -S base base-devel --noconfirm
-yes | sudo pacman -S git svn git-lfs jq tmux neovim zsh zsh-completions imagemagick libmagick wget man arch-wiki-lite --noconfirm
-sudo pacman -S gcc xz ncurses glu mesa wxgtk2 libpng oniguruma libssh unixodbc binutils make fakeroot autoconf automake bison freetype2 gettext icu krb5 libedit libjpeg libpng libxml2 libzip pkg-config re2c zlib unzip openssl-1.0 ctags ncurses ack the_silver_searcher fontconfig libmagick6 --noconfirm
-sudo curl -O https://blackarch.org/strap.sh
-sudo sha1sum strap.sh
-sudo chmod +x strap.sh
-yes | sudo ./strap.sh --noconfirm
+yes | sudo pacman -S git svn git-lfs jq tmux neovim zsh zsh-completions imagemagick libmagick wget man arch-wiki-lite jdk-openjdk kotlin rust npm yarn ruby --noconfirm
+sudo pacman -S gcc xz ncurses glu mesa wxgtk2 libpng oniguruma libssh unixodbc binutils make fakeroot autoconf automake bison freetype2 gettext icu krb5 libedit libjpeg libpng libxml2 libzip pkg-config re2c zlib unzip openssl-1.0 ctags ncurses ack the_silver_searcher fontconfig exa tldr dust fd tokei procs hyperfine skim sd bottom bat --noconfirm
+curl -O https://blackarch.org/strap.sh
+echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
+chmod +x strap.sh
+sudo ./strap.sh
 sudo rm -rf ~/strap.sh
 yes | sudo pacman -Syuu --noconfirm 
 yes | sudo pacman -S yay --noconfirm 
 sudo runuser -l vagrant -c 'yes | yay -Syuu --devel --timeupdate --noconfirm' 
-sudo runuser -l vagrant -c 'yes | yay -S devtools asdf-vm rcm heroku-cli bfg universal-ctags-git --noconfirm' 
+sudo runuser -l vagrant -c 'yes | yay -S devtools asdf-vm rcm heroku-cli bfg universal-ctags-git zsh-theme-powerlevel10k-git --noconfirm' 
 yes | sudo pacman -S docker docker-compose --noconfirm
 sudo runuser -l vagrant -c 'chsh -s $(which zsh)'
 sudo runuser -l vagrant -c 'git clone git://github.com/thoughtbot/dotfiles.git ~/dotfiles'
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
