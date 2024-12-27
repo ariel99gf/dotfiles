30deba33ef98240c7a489d793333724cb784b612
Ariel Fonseca <ariel99gf@gmail.com>
2024-12-27 14:28:11 -0300

    using fzf-git

--- arch.sh
+++ arch.sh
@@ -9,7 +9,7 @@ sudo reflector --country 'United States' --latest 200 --protocol https --age 24 
 rm -f /etc/pacman.d/mirrorlist.pacnew
 yes | sudo pacman -S base base-devel --noconfirm
 yes | sudo pacman -S git svn git-lfs jq tmux neovim zsh zsh-completions imagemagick libmagick wget man arch-wiki-lite jdk-openjdk kotlin rust npm yarn ruby ansible terraform --noconfirm
-yes | sudo pacman -S gcc xz ncurses glu mesa libpng oniguruma libssh unixodbc binutils make fakeroot autoconf zip automake bison freetype2 gettext icu krb5 libedit libjpeg libpng libxml2 libzip pkg-config re2c zlib unzip openssl ctags ncurses ack the_silver_searcher fontconfig exa tldr dust fd tokei procs hyperfine skim sd bottom bat fzf --needed
+yes | sudo pacman -S gcc xz ncurses glu mesa libpng oniguruma libssh unixodbc binutils make fakeroot autoconf zip automake bison freetype2 gettext icu krb5 libedit libjpeg libpng libxml2 libzip pkg-config re2c zlib unzip openssl ctags ncurses ack the_silver_searcher fontconfig exa tldr dust fd tokei procs hyperfine skim sd bottom bat --needed
 curl -O https://blackarch.org/strap.sh
 echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
 chmod +x strap.sh
@@ -18,7 +18,7 @@ sudo rm -rf ~/strap.sh
 yes | sudo pacman -Syuu --noconfirm
 cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chown -R "$USER:$USER" ./yay-git && cd yay-git && makepkg -si && yay -Syu --devel --timeupdate
 yes | yay -Syuu --devel --timeupdate --noconfirm
-yes | yay -S devtools asdf-vm rcm bfg universal-ctags-git zsh-theme-powerlevel10k-git --noconfirm
+yes | yay -S devtools asdf-vm rcm bfg universal-ctags-git zsh-theme-powerlevel10k-git fzf-git --noconfirm
 curl -s "https://get.sdkman.io" | bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl https://mise.run | sh
